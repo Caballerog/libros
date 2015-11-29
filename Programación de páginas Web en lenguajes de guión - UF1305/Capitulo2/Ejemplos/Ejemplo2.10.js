6bca26803a948b1a99ee6ecf539f7225e45289e0
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2015-11-29 13:32:29 +0100

    Ejemplos del Capítulo 2

---
+++ Programación de páginas Web en lenguajes de guión - UF1305/Capitulo2/Ejemplos/Ejemplo2.10.js
@@ -0,0 +1,4 @@
+var iva = 21;
+var mensaje = "Bienvenido a nuestra tienda Web";
+var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+var ivaIncluido = true;
---
+++ Programación de páginas Web en lenguajes de guión - UF1305/Capitulo2/Ejemplos/Ejemplo2.11.js
@@ -0,0 +1,4 @@
+var iva = 21;
+var precio = 200;
+var valor;
+valor = precio * (iva / 100);
