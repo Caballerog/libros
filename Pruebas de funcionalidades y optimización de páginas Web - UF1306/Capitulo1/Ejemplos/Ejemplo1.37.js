bf4c352ffcf1b51e7d481f239723486d8d3e6d02
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2015-12-25 21:38:53 -0800

    Ejemplos del capítulo 1

--- Pruebas de funcionalidades y optimización de páginas Web - UF1306/Capitulo1/Ejemplos/Ejemplo1.37.js
+++ Pruebas de funcionalidades y optimización de páginas Web - UF1306/Capitulo1/Ejemplos/Ejemplo1.37.js
@@ -2,5 +2,5 @@ function testFecha(campo) {
     var valor = campo.value;
     var expresión = "(^(((0[1-9]|1\\d|2[0-8])\/(0[1-9]|1[0-2]))|((31\/(0[13578]|1[02])) | ((29|30)\/(0[1,3-9]|1[0-2]))))\/\\d{4}$)|(^(29(\/)02)(\/)((0[48]00| [13579][26]00| [2468][048]00) |(\\d{2})?(0[48]|[2468][048]|[13579][26]))$)";
     var rExp = new RegExp(expresion);
-    return rExp.test(valor);
+    return rExp.test(valor); 
 }
