b77223e0d52ce083f99e60b7b44e2bdf342afc27
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2016-01-05 11:15:51 +0100

    Ejemplos del Capítulo 1

---
+++ Pruebas de funcionalidades y optimización de páginas Web - UF1306/Capitulo1/Ejemplos/Ejemplo1.47.js
@@ -0,0 +1,18 @@
+var clearRender = function(e){
+  nodo=window.document.getElementById(e).getElementsByTagName('span')[0];
+  if (nodo && nodo.parentNode) {
+       nodo.parentNode.removeChild(nodo);
+   }
+}
+var renderError = function(e) {
+  clearRender(e);
+  var span = window.document.createElement('span');
+  span.innerHTML = "<img src='img/bad.png'/>";
+  window.document.getElementById(e).appendChild(span);
+}
+var renderOk = function(e) {
+  clearRender(e);
+  var span = window.document.createElement('span');
+  span.innerHTML = "<img src='img/ok.png'/>";
+  window.document.getElementById(e).appendChild(span);
+}
